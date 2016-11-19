ed215cb847e21ee3a9647c952a9b5792a046f84a
Zach Beaird <zbbeaird1989@gmail.com>
2016-11-19 13:53:48 -0600

    Testing Board Class

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,8 +1,22 @@
 module TicTacToe
 	class Board
 		attr_accessor :grid
-		def intialize(input = {})
+		def initialize(input = {})
 			@grid = input.fetch(:grid, default_grid)
+		end
+
+		def get_cell(x, y)
+			grid[y][x]
+		end
+
+		def set_cell(x, y, value)
+			get_cell(x, y).value = value
+		end
+
+		def game_over
+			return :winner if winner?
+			return :draw if draw?
+			false
 		end
 
 		private
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,47 @@
+require "spec_helper"
+
 module TicTacToe
 	describe Board do 
 		context "#initialize" do 
 			it "passes when initialized with a hash that has a grid key" do
 				expect { Board.new({ grid: "grid" }) }.to_not raise_error
+			end
+
+			it "creates 3 rows by default" do 
+				board = Board.new
+				expect(board.grid.length).to eq 3
+			end
+
+			it "creates 3 columns by default" do 
+				board = Board.new
+				board.grid.each do |row|
+					expect(row.length).to eq 3
+				end
+			end
+		end
+
+		context "#grid" do 
+			it "returns the grid" do 
+				board = Board.new(grid: "blah")
+				expect(board.grid).to eq "blah"
+			end
+		end
+
+		context "#get_cell" do 
+			it "returns the cell at the given coordinates" do 
+				grid = [["", "", ""], ["", "", "Hello"], ["", "", ""]]
+				board = Board.new(grid: grid)
+				expect(board.get_cell(2, 1)).to eq "Hello"
+			end
+		end
+
+		context "#set_cell" do 
+			it "updates the value of a cell" do 
+				Cat = Struct.new(:value)	
+				grid = [[Cat.new("Hello"), "", ""], ["", "", ""], ["", "", ""]]
+				board = Board.new(grid: grid)
+				board.set_cell(0, 0, "Meow")
+				expect(board.get_cell(0, 0).value).to eq "Meow"
 			end
 		end
 	end

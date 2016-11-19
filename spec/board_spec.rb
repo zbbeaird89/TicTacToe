b77c47788f749860af48f93675ff504f5f537a5c
Zach Beaird <zbbeaird1989@gmail.com>
2016-11-19 14:19:46 -0600

    Testing game_over method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -44,5 +44,27 @@ module TicTacToe
 				expect(board.get_cell(0, 0).value).to eq "Meow"
 			end
 		end
+
+		context "#game_over" do 
+			it "returns :winner if winner? is true" do 
+				board = Board.new
+				board.stub(:winner?) { true }
+				expect(board.game_over).to eq :winner
+			end
+
+			it "returns :draw if winner? is false and draw? is true" do 
+				board = Board.new
+				board.stub(:winner?) { false }
+				board.stub(:draw?) { true }
+				expect(board.game_over).to eq :draw
+			end
+
+			it "returns false if winner? is false and draw? is false" do 
+				board = Board.new
+				board.stub(:winner?) { false }
+				board.stub(:draw?) { false }
+				expect(board.game_over).to eq false
+			end
+		end
 	end
 end
